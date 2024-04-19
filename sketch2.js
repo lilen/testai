0547c86b79fd71f30c8b1667971f320a9d581033
lilen <lilen@shsh.ylc.edu.tw>
2024-04-19 14:49:40 +0800

    Update sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -51,4 +51,5 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+  ThunkableWebviewerExtension.postMessage(label);
 }
